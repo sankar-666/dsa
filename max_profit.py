e5d67c74350814275c592a691e1babcec3559714
sankar <sankusanku001@gmail.com>
2026-08-24 22:48:21 +0100

    revision: max_profit

--- max_profit.py
+++ max_profit.py
@@ -1,14 +1,27 @@
+# def max_profit(prices):
+#     min_price = float("inf")
+#     profit = 0
+
+#     for price in prices:
+#         if price < min_price:
+#             min_price = price
+
+#         current_profit = price - min_price
+#         profit = max(profit, current_profit)
+
+#     return profit
+
+# print(max_profit([7, 6, 4, 3, 1]))
+
 def max_profit(prices):
-    min_price = float("inf")
     profit = 0
+    buy_price = prices[0]
 
     for price in prices:
-        if price < min_price:
-            min_price = price
+        buy_price = min(buy_price, price)
 
-        current_profit = price - min_price
-        profit = max(profit, current_profit)
+        profit = max(profit, price-buy_price)
 
-    return profit
+    return profit 
 
-print(max_profit([7, 6, 4, 3, 1]))
+print(max_profit([7, 1, 4, 3, 5]))
