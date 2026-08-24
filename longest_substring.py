b5684da3d85dd445fdf93de73a01291879578385
sankar <sankusanku001@gmail.com>
2026-08-24 23:06:21 +0100

    revision: length_of_longest_substring

--- longest_substring.py
+++ longest_substring.py
@@ -1,20 +1,41 @@
-# T(On) S(On
-# eg s = "abcabcbb" out=3
+# # T(On) S(On
+# # eg s = "abcabcbb" out=3
+
+# def length_of_longest_substring(s):
+#     seen = {}
+#     left = 0
+#     longest = 0
+
+#     for i, ch in enumerate(s):
+#         if ch in seen and seen[ch] >= left:
+#             left = seen[ch] + 1
+
+#         seen[ch] = i
+
+#         current_window = i + 1 - left
+#         longest = max(longest, current_window)
+
+#     return longest
+
+# print(length_of_longest_substring("abcabcb"))
+
 
 def length_of_longest_substring(s):
+
     seen = {}
-    left = 0
     longest = 0
+    left = 0
 
-    for i, ch in enumerate(s):
-        if ch in seen and seen[ch] >= left:
-            left = seen[ch] + 1
+    for i, char in enumerate(s):
+        if char in seen and seen[char] >= left:
+            left = seen[char] + 1
 
-        seen[ch] = i
+        seen[char] = i
+        longest =max(longest, i + 1 - left)
 
-        current_window = i + 1 - left
-        longest = max(longest, current_window)
 
     return longest
+
+
 
 print(length_of_longest_substring("abcabcb"))
