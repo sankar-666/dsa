4f038705bd00600c3ad4897940eead980808f74a
sankar <sankusanku001@gmail.com>
2026-08-24 23:20:38 +0100

    revision: valid parentheses

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,21 +1,46 @@
+# def valid_parentheses(s):
+#     stack = []
+
+#     for ch in s:
+#         if ch in "([{":
+#             stack.append(ch)
+#             continue
+
+#         if not stack:
+#             return False
+
+#         top = stack.pop()
+
+#         if (
+#             (top == "(" and ch != ")") or
+#             (top == "[" and ch != "]") or
+#             (top == "{" and ch != "}")
+#         ):
+#             return False
+
+#     return len(stack) == 0
+
 def valid_parentheses(s):
+
     stack = []
 
     for ch in s:
-        if ch in "([{":
+        if ch in "{([":
             stack.append(ch)
             continue
 
-        if not stack:
-            return False
+        if stack:
+            top = stack.pop()
 
-        top = stack.pop()
+            if  (
+                    (top == "(" and ch != ")") or
+                    (top == "{" and ch != "}") or
+                    (top == "[" and ch != "]") 
+                ):
+                return False
 
-        if (
-            (top == "(" and ch != ")") or
-            (top == "[" and ch != "]") or
-            (top == "{" and ch != "}")
-        ):
-            return False
 
     return len(stack) == 0
+
+
+print(valid_parentheses("({[]})"))
